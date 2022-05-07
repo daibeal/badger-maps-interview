c39056565f614e6522d9cf77b81d8176e4c5004e
daibeal <57094326+daibeal@users.noreply.github.com>
2022-05-07 03:05:39 +0200

    Added header

---
+++ __main__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = "Dairon Andrés Benites ALdaz"
+__copyright__ = "Copyright 2022, @daibeal"
+__license__ = "GPL"
+__version__ = "1.0.1"
+__maintainer__ = "Dairon Andrés Benites ALdaz"
+__email__ = "contact@andresbenites.es"
+__status__ = "Production"
